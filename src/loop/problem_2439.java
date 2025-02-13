9d700428dea4e69eb9f3f1fe0f9b51bbc342cc1f
Kim Ji Ho <2022158007@tukorea.ac.kr>
2025-02-13 18:09:08 +0900

    반복문: 별 찍기 - 2

---
+++ src/loop/problem_2439.java
@@ -0,0 +1,23 @@
+package loop;
+
+import java.io.*;
+
+public class problem_2439 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
