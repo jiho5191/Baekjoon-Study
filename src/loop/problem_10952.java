9b294538a5319ba5a87a71a391c35ec893dc4582
Kim Ji Ho <2022158007@tukorea.ac.kr>
2025-02-13 18:40:57 +0900

    반복문: A+B(0 0을 입력하면 종료)

---
+++ src/loop/problem_10952.java
@@ -0,0 +1,25 @@
+package loop;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class problem_10952 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        while (true) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0) {
+                bw.flush();
+                return;
+            }
+
+            bw.write((a+b) + "\n");
+        }
+    }
+}
