dec667910403afa29d3581126757eef48b8ad95e
Kim Ji Ho <2022158007@tukorea.ac.kr>
2025-02-24 15:35:07 +0900

    문자열: 단어 길이 재기

---
+++ src/string/problem_2743.java
@@ -0,0 +1,12 @@
+package string;
+
+import java.io.*;
+
+// 단어 길이 재기
+public class problem_2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str.length());
+    }
+}
